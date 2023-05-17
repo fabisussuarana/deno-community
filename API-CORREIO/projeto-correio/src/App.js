306bbfe71b5e0e0eb7df5535f5b438a106ee7c82
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2023-05-16 23:49:18 -0500

    api correios

---
+++ API-CORREIO/projeto-correio/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Form from './components/formulario/form';
+
+function App() {
+  return (
+    <React.StrictMode>
+      {/* <Form /> */}
+    </React.StrictMode>
+  );
+}
+
+export default App;
---
+++ API-CORREIO/projeto-correio/src/components/formulario/form.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Formulario = () => {
+    return (
+        <>
+            <h1>Api de correios</h1>
+
+            <form action="">
+                <label>Digite o seu cep: </label>
+                <input type="text" />
+            </form>
+        </>
+    )
+}
+
+export default Formulario;
